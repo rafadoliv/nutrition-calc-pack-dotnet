953c654e1de82fbaf3b1c8aa77c2b8d40c0f59f2
rafadoliv <rafaeloliveira.lp@hotmail.com>
2022-11-23 20:56:10 -0300

    Criação do pacote final Nuget, versão alterada de 1.0.0 para 1.1.0

--- src/health-calc-dotnet/health-calc-pack-dotnet/obj/Debug/net6.0/health-calc-pack-dotnet.AssemblyInfo.cs
+++ src/health-calc-dotnet/health-calc-pack-dotnet/obj/Debug/net6.0/health-calc-pack-dotnet.AssemblyInfo.cs
@@ -13,11 +13,11 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("health-calc-pack-dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.1.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("health-calc-pack-dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("health-calc-pack-dotnet")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.1.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
 
